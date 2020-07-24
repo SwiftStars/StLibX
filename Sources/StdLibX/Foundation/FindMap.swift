1db869f9cff08d7e208332bd3433df477065beef
Ben Sova <65474198+BenSova@users.noreply.github.com>
2020-07-24 14:52:47 -0600

    Refine FindMap

--- Sources/StdLibX/Foundation/FindMap.swift
+++ Sources/StdLibX/Foundation/FindMap.swift
@@ -7,30 +7,6 @@ import Foundation
 
 extension Sequence {
 
-    /// Find an element with a certain value a certain spot.
-    ///
-    /// `Sequence.findMap(_:in:)` looks for an element in a Sequence that, when passed into `grabItem(_:)` (aka `in`), matches `find`. If nothing is found `findMap` will return `nil`.
-    ///
-    /// When the `preferFirst` argument is passed in as `false`, the later element that matches the criteria is used.
-    ///
-    /// - Parameters:
-    ///   - find: The item to compare to the one found in an element.
-    ///   - grabItem: A func to convert an element to an item that can be compared to the `find` option.
-    ///   - preferFirst: Whether the first element should be used (`true`) or the last one (`false`).
-    /// - Returns: The element that contains `find`.
-    /// # Declared in
-    ///  [FindMap.swift](https://github.com/SwiftStars/StdLibX/tree/master/Sources/StdLibX/Foundation/FindMap.swift)
-    ///
-    public func findMap<T: Equatable>(_ find: T, at grabItem: @escaping (Element) -> T, preferFirst: Bool = true) -> Element? {
-        var returnItem: Element?
-        forEach { (item) in
-            if (returnItem != nil || !preferFirst) && grabItem(item) == find {
-                returnItem = item
-            }
-        }
-        return returnItem
-    }
-
     /// Find all elements with a certain value a certain spot.
     ///
     /// `Sequence.findMap(_:in:)` looks for all elements in a Sequence that, when passed into `grabItem(_:)` (aka `in`), matches `find`. If nothing is found `findMap` will return `[]`.
@@ -63,7 +39,7 @@ extension Sequence {
     /// # Declared in
     ///  [FindMap.swift](https://github.com/SwiftStars/StdLibX/tree/master/Sources/StdLibX/Foundation/FindMap.swift)
     ///
-    public func findMap<T: Equatable>(_ find: T, at grabItem: (Element) -> T) -> Bool {
+    public func containsAt<T: Equatable>(_ find: T, at grabItem: (Element) -> T) -> Bool {
         return map(grabItem).contains(find)
     }
 
--- Tests/StdLibXTests/FindMapTests.swift
+++ Tests/StdLibXTests/FindMapTests.swift
@@ -10,22 +10,16 @@ import XCTest
 class FindMapTests: XCTestCase {
 
     func testFindMap() throws {
-        print("\n=--Sequence FindMap Tests----------------------------------=\n")
+        print("\n=--Sequence FindMap/ContainsAt Tests-----------------------=\n")
         let simpleTupleSequence = [(one: "String", two: 10), (one: "Int", two: 34), (one: "Float", two: 34), (one: "Double", two: 13), (one: "String", two: 135), (one: "Find Me!", two: 100)]
         print("Simple Tuple Sequence: \(simpleTupleSequence)")
-        let findMeTuplesBool: Bool = simpleTupleSequence.findMap("Find Me!", at: \.one)
+        let findMeTuplesBool = simpleTupleSequence.containsAt("Find Me!", at: \.one)
         XCTAssert(
             findMeTuplesBool,
             false: "Unable to find \"Find Me!\" under \"one\" in \"simpleTupleSequence\" using \"-> Bool\" (Got: \"\(findMeTuplesBool)\")",
             true: "Found \"Find Me!\" under \"one\" in \"simpleTupleSequence\" using \"-> Bool\" (Proof: \"\(findMeTuplesBool)\")"
         )
-        let findMeTuplesElement: (one: String, two: Int)? = simpleTupleSequence.findMap("Find Me!", at: \.one)
-        XCTAssert(
-            (findMeTuplesElement ?? (one: "BOO", two: -1)) == (one: "Find Me!", two: 100),
-            false: "Unable to find \"Find Me!\" under \"one\" in \"simpleTupleSequence\" using \"-> Element\" (Got: \"\(String(describing: findMeTuplesElement))\")",
-            true: "Found \"Find Me!\" under \"one\" in \"simpleTupleSequence\" using \"-> Element\" (Proof: \"\(String(describing: findMeTuplesElement))\")"
-        )
-        let findMeTuplesElements: [(one: String, two: Int)] = simpleTupleSequence.findMap("Find Me!", at: \.one)
+        let findMeTuplesElements = simpleTupleSequence.findMap("Find Me!", at: \.one)
         XCTAssert(
             findMeTuplesElements[0] == (one: "Find Me!", two: 100) && findMeTuplesElements.count == 1,
             false: "Unable to find \"Find Me!\" under \"one\" in \"simpleTupleSequence\" using \"-> [Element]\" (Got: \"\(findMeTuplesElements)\")",
