d62da917308ffaa0c208ada46271f2a517df9d44
Ben Sova <65474198+BenSova@users.noreply.github.com>
2020-07-28 15:18:39 -0600

    Add whereAt to get the index of an item.

--- Sources/StdLibX/Foundation/FindMap.swift
+++ Sources/StdLibX/Foundation/FindMap.swift
@@ -30,7 +30,7 @@ extension Sequence {
 
     /// Find an element with a certain value in a certain spot.
     ///
-    /// `Sequence.findMap(_:in:)` looks for all elements in a Sequence that, when passed into `grabItem(_:)` (aka `in`), matches `find`. If nothing is found `findMap` will return `false`, otherwise `true`.
+    /// `Sequence.containsAt(_:in:)` looks for all elements in a Sequence that, when passed into `grabItem(_:)` (aka `in`), matches `find`. If nothing is found `findMap` will return `false`, otherwise `true`.
     ///
     /// - Parameters:
     ///   - find: The item to compare to the one found in an element.
@@ -42,5 +42,31 @@ extension Sequence {
     public func containsAt<T: Equatable>(_ find: T, at grabItem: (Element) -> T) -> Bool {
         return map(grabItem).contains(find)
     }
+}
 
+extension Array {
+    /// Find the location of the first element with a certain value at a certain spot
+    ///
+    /// `Array.whereFrom(_:in:)` looks for all elements in an Array that, when passed into `grabItem(_:)` (aka `in`), matches `find`. If nothing is found `findMap` will return `nil`.
+    ///
+    /// - Parameters:
+    ///   - find: The item to compare to the one found in an element.
+    ///   - grabItem: A func to convert an element to an item that can be compared to the `find` option.
+    /// - Returns: The location of the element that contains `find`.
+    /// # Declared in
+    ///  [FindMap.swift](https://github.com/SwiftStars/StdLibX/tree/master/Sources/StdLibX/Foundation/FindMap.swift)
+    ///
+    public func whereAt<T: Equatable>(_ find: T, at grabItem: (Element) -> T) -> Int? {
+        var status = (found: false, index: 0)
+        forEach { (item) in
+            if !(status.found) {
+                if grabItem(item) == find {
+                    status.found = true
+                } else {
+                    status.index += 1
+                }
+            }
+        }
+        return status.index
+    }
 }
